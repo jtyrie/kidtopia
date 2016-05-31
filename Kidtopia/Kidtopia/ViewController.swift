93df2f30fb4821d09a5b40e2d26194f11a80ba30
jtyrie <jacob.tyrie@sky.uk>
2016-05-31 13:57:50 +0100

    Merge header code with logic for play

--- Kidtopia/Kidtopia/ViewController.swift
+++ Kidtopia/Kidtopia/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let cardDeck = CardDeck()
+    private var lastSelectedCard: Card?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,18 +22,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
 
 extension ViewController: UICollectionViewDelegate {
 
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
         guard let cell = collectionView.cellForItemAtIndexPath(indexPath) as? BuddyCollectionViewCell else {
             return
         }
         cell.flipCardUp()
+        
+        guard let card = cardDeck.cardAtIndex(indexPath.row) else {
+            return
+        }
+        
+        if lastSelectedCard?.isEqualToCard(card) == true {
+            // Splat
+        } else {
+            // Flip back
+        }
     }
 
 }
@@ -41,7 +55,7 @@ extension ViewController: UICollectionViewDataSource {
     func collectionView(collectionView: UICollectionView,
                         numberOfItemsInSection section: Int) -> Int {
         
-        return 6
+        return cardDeck.count()
     }
     
     func collectionView(collectionView: UICollectionView,
