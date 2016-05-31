9f8cda56b4b92257a9c3234533771f0f2aaa0741
Sam Meadley <sam.meadley@gmail.com>
2016-05-31 11:29:28 +0100

    Adds basic grid structure
    We'll use a collection view as we get a bunch of stuff for free. WIP commit so Jake can pick up designing the cells.

---
+++ Kidtopia/Kidtopia/BuddyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BuddyCollectionViewCell.swift
+//  Kidtopia
+//
+//  Created by Meadley, Samuel (Developer) on 31/05/2016.
+//  Copyright © 2016 Sky. All rights reserved.
+//
+
+import UIKit
+
+class BuddyCollectionViewCell: UICollectionViewCell {
+    
+    static let defaultReuseIdentifier = String(BuddyCollectionViewCell)
+}
